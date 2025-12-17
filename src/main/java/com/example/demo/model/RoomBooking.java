118b0ac5aa7c95ab23cc519af2ecc77d2fd3e088
bala-amypo <bala@amypo.in>
2025-12-17 06:40:38 +0000

    User commit

--- src/main/java/com/example/demo/model/RoomBooking.java
+++ src/main/java/com/example/demo/model/RoomBooking.java
@@ -1,6 +1,11 @@
 import jakarta.persistence.Entity;
-kimp
+import jakarta.persistence.Id;
 @Entity
 public class RoomBooking{
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    private String roomNumber;
+    private Boolean
     
 }
