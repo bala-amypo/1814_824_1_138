01256cbc0ead8bb209f67fd7049df7c3478ed963
bala-amypo <bala@amypo.in>
2025-12-18 05:21:35 +0000

    User commit

--- src/main/java/com/example/demo/service/GuestService.java
+++ src/main/java/com/example/demo/service/GuestService.java
@@ -1,5 +1,8 @@
 package com.example.demo.service.impl;
+import com.example.demo.service.GuestService;
+import org.springframework
+@Service
+public class GuestServiceImpl implements GuestService{
+    @Autowired
 
-public class GuestServiceImpl{
-    
 }
