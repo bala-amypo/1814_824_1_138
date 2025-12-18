3dfc1f6f8514c33f2b1b8157384a431bc55216d8
bala-amypo <bala@amypo.in>
2025-12-18 08:21:34 +0000

    User commit

--- src/main/java/com/example/demo/controller/GuestController.java
+++ src/main/java/com/example/demo/controller/GuestController.java
@@ -1,5 +1,10 @@
 package com.example.demo.controller;
-import org.springframework.web.bind
+import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Authowired;
+import  com.example.demo.model.Guest;
+import com.example.demo.service.GuestService;
+@RestController
 public class GuestController{
-    
+    @Autowired
+    Guest
 }
