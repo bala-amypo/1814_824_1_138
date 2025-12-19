bcb5eaad990b92188afc505fe2163dee83d5119f
bala-amypo <bala@amypo.in>
2025-12-19 05:00:33 +0000

    User commit

--- src/main/java/com/example/demo/repository/AccessLogRepository.java
+++ src/main/java/com/example/demo/repository/AccessLogRepository.java
@@ -1,9 +1,7 @@
 package com.example.demo.repository;
 
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.example.demo.model.AccessLog;
 
 public interface AccessLogRepository extends JpaRepository<AccessLog, Long> {
--- src/main/java/com/example/demo/service/impl/AccessLogServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/AccessLogServiceImpl.java
@@ -1,33 +1,24 @@
 package com.example.demo.service.impl;
 
 import java.util.List;
-
 import org.springframework.stereotype.Service;
 
 import com.example.demo.model.AccessLog;
 import com.example.demo.repository.AccessLogRepository;
-import com.example.demo.repository.KeyRepository;
 import com.example.demo.service.AccessLogService;
 
 @Service
 public class AccessLogServiceImpl implements AccessLogService {
 
     private final AccessLogRepository accessLogRepository;
-    private final KeyRepository keyRepository;
 
-    public AccessLogServiceImpl(AccessLogRepository accessLogRepository,
-                                KeyRepository keyRepository) {
+    public AccessLogServiceImpl(AccessLogRepository accessLogRepository) {
         this.accessLogRepository = accessLogRepository;
-        this.keyRepository = keyRepository;
     }
 
     @Override
     public AccessLog createLog(AccessLog log) {
-        if (keyRepository.existsById(log.getKeyId())) {
-            log.setResult("SUCCESS");
-        } else {
-            log.setResult("DENIED");
-        }
+        log.setResult("SUCCESS");
         return accessLogRepository.save(log);
     }
 
