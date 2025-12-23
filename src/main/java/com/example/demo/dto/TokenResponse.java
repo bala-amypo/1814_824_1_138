307a5a2cd4964c101cb3999b65586602ac258c87
bala-amypo <bala@amypo.in>
2025-12-23 15:09:26 +0000

    User commit

--- src/main/java/com/example/demo/dto/TokenResponse.java
+++ src/main/java/com/example/demo/dto/TokenResponse.java
@@ -1,4 +1,31 @@
-public class TokenResponse
-{
+package com.example.demo.dto;
+
+public class TokenResponse {
+
+    private String token;
+    private String tokenType = "Bearer";
+
+    // ✅ No-args constructor
+    public TokenResponse() {
+    }
+
     
+    public TokenResponse(String token) {
+        this.token = token;
+    }
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getTokenType() {
+        return tokenType;
+    }
+
+    public void setTokenType(String tokenType) {
+        this.tokenType = tokenType;
+    }
 }
