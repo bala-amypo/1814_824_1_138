6630446cf8dc787401d655f777a33305b2393666
bala-amypo <bala@amypo.in>
2025-12-25 14:58:15 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,44 +1,27 @@
-package com.example.demo.controller;
+package com.example.demo.auth.controller;
 
-import com.example.demo.model.Guest;
-import com.example.demo.security.JwtTokenProvider;
-import com.example.demo.service.GuestService;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
+import com.example.demo.auth.service.AuthService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/api/auth")
 public class AuthController {
 
-    private final AuthenticationManager authenticationManager;
-    private final JwtTokenProvider tokenProvider;
-    private final GuestService guestService;
+    private final AuthService authService;
 
-    public AuthController(AuthenticationManager authenticationManager,
-                          JwtTokenProvider tokenProvider,
-                          GuestService guestService) {
-        this.authenticationManager = authenticationManager;
-        this.tokenProvider = tokenProvider;
-        this.guestService = guestService;
-    }
-
-    @PostMapping("/register")
-    public Guest register(@RequestBody Guest guest) {
-        return guestService.createGuest(guest);
+    public AuthController(AuthService authService) {
+        this.authService = authService;
     }
 
     @PostMapping("/login")
     public String login(@RequestParam String email,
                         @RequestParam String password) {
+        return authService.login(email, password);
+    }
 
-        Authentication authentication =
-                authenticationManager.authenticate(
-                        new UsernamePasswordAuthenticationToken(email, password)
-                );
-
-        return tokenProvider.generateToken(authentication);
+    @PostMapping("/register")
+    public String register(@RequestParam String email,
+                           @RequestParam String password) {
+        return authService.register(email, password);
     }
 }
-    
