d2120070744fb213b494fbaebcb8261ec334506b
bala-amypo <bala@amypo.in>
2025-12-26 04:06:09 +0000

    User commit

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -15,41 +15,41 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @Configuration
 public class SecurityConfig {
 
-    @Autowired
-    private JwtAuthenticationFilter jwtAuthenticationFilter;
+    // @Autowired
+    // private JwtAuthenticationFilter jwtAuthenticationFilter;
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    // @Bean
+    // public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        http
-            .csrf(csrf -> csrf.disable())
-            .sessionManagement(sm ->
-                    sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .authorizeHttpRequests(auth -> auth
-                    .requestMatchers(
-                            "/auth/**",
-                            "/v3/api-docs/**",
-                            "/swagger-ui/**",
-                            "/swagger-ui.html"
-                    ).permitAll()
-                    .anyRequest().authenticated()
-            );
+    //     http
+    //         .csrf(csrf -> csrf.disable())
+    //         .sessionManagement(sm ->
+    //                 sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+    //         .authorizeHttpRequests(auth -> auth
+    //                 .requestMatchers(
+    //                         "/auth/**",
+    //                         "/v3/api-docs/**",
+    //                         "/swagger-ui/**",
+    //                         "/swagger-ui.html"
+    //                 ).permitAll()
+    //                 .anyRequest().authenticated()
+    //         );
 
-        http.addFilterBefore(
-                jwtAuthenticationFilter,
-                UsernamePasswordAuthenticationFilter.class);
+    //     http.addFilterBefore(
+    //             jwtAuthenticationFilter,
+    //             UsernamePasswordAuthenticationFilter.class);
 
-        return http.build();
-    }
+    //     return http.build();
+    // }
 
-    @Bean
-    public AuthenticationManager authenticationManager(
-            AuthenticationConfiguration config) throws Exception {
-        return config.getAuthenticationManager();
-    }
+    // @Bean
+    // public AuthenticationManager authenticationManager(
+    //         AuthenticationConfiguration config) throws Exception {
+    //     return config.getAuthenticationManager();
+    // }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+    // @Bean
+    // public PasswordEncoder passwordEncoder() {
+    //     return new BCryptPasswordEncoder();
+    // }
 }
